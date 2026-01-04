f63dbf68724f488239367bd901f1fbd688e8c50e
Mkostadinoski <mirekostadinoski@gmail.com>
2026-01-04 22:34:28 +0100

    removed health

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,16 @@
 <?php
 
-// use Illuminate\Support\Facades\Route;
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 use Illuminate\Support\Facades\Route;
 
-Route::get('/health', function () {
-    return response()->json([
-        'status' => 'ok',
-        'time' => now()->toDateTimeString()
-    ]);
+Route::get('/', function () {
+    return view('welcome');
 });
+
+// use Illuminate\Support\Facades\Route;
+
+// Route::get('/health', function () {
+//     return response()->json([
+//         'status' => 'ok',
+//         'time' => now()->toDateTimeString()
+//     ]);
+// });
